d3a2f484b7565063183ac221cc4e341eb66b908b
Nishant <ntih5565@gmail.com>
2024-02-01 16:58:30 +0530

    First Push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './Components/Header';
+import Home from './Components/Home';
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
