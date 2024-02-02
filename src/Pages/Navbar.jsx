f7cb60d1a33036c1ceb9e55db4c196f467a6c4b2
Arpan-9911 <arpanks1263@gmail.com>
2024-02-03 00:03:48 +0530

    final navbar

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import DesktopNav from '../Components/Navbar/DesktopNav'
 import MobileNav from '../Components/Navbar/MobileNav'
+import './Navbar.css'
 import { useEffect , useState} from 'react'
 
 const Navbar = () => {
