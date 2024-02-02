851d7dd22f8530cd37844e26d0b6c9a0d29923bc
Nishant <ntih5565@gmail.com>
2024-02-03 00:52:22 +0530

    FIxed Navbar Styling

--- src/Components/Navbar/DesktopNav.jsx
+++ src/Components/Navbar/DesktopNav.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom'
 
 const DesktopNav = () => {
 
-  const linkStyle = "mx-5 inline-block no-underline text-2xl font-bold text-white transition-all duration-500 hover:bg-white hover:text-blue-500 hover:px-2 hover:rounded-lg";
+  const linkStyle = "mx-5 inline-block no-underline text-2xl font-bold text-white transition-all duration-500 hover:bg-white hover:text-blue-500 hover:px-2 hover:rounded-lg mddmax:text-lg mddmax:mx-3";
 
   return (
     <>
-        <div className='flex justify-between items-center h-24 bg-black p-8 z-10' id='nav-desk-body'>
-        <div className="logo text-6xl text-white p-3 rounded-lg z-10" id='nav-desk-logo'>
+        <div className='flex justify-between items-center h-24 bg-black p-8 z-10 mddmax:h-20 mddmax:p-5' >
+        <div className="logo text-6xl text-white p-3 rounded-lg z-10 mddmax:text-4xl mddmax:p-1" >
           Antara
         </div>
         <nav className="nav-bar">
             <ul className="flex text-blue-500">
-                <Link to={''} className={linkStyle} id='nav-link'> Home </Link>
-                <Link to={''} className={linkStyle} id='nav-link'> Events </Link>
-                <Link to={''} className={linkStyle} id='nav-link'> Timeline </Link>
-                <Link to={''} className={linkStyle} id='nav-link'> Contact Us </Link>
+                <Link to={''} className={linkStyle}> Home </Link>
+                <Link to={''} className={linkStyle}> Events </Link>
+                <Link to={''} className={linkStyle}> Timeline </Link>
+                <Link to={''} className={linkStyle}> Contact Us </Link>
             </ul>
         </nav>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '425px',
+        'md': '764px',
+        'lg': '1024px',
+        'xl': '1280px',
+        'smmax': {'max': '425px'},
+        'mdmax': {'max': '764px'},
+        'mdsmax': {'max': '874px'},
+        'mddmax':{'max':'900px'},
+        'lgmax': {'max': '1024px'},
+        'xlmax': {'max': '1155px'},
+        'xxlmax': {'max': '1280px'},
+        'xxxlmax':{'max':'1345px'},
+      },
       fontFamily: {
         'logo': ['Berkshire Swash', 'serif'],
       },
