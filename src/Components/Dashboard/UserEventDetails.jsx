59d3ec6dc442a35cdd3a94909c5a48eba20dec4f
pratyush-jh <jhapratyush002@gmail.com>
2024-02-09 01:04:59 +0530

    Dashboard Half Done

--- src/Components/Dashboard/UserEventDetails.jsx
+++ src/Components/Dashboard/UserEventDetails.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import './dashboard.css';
 
 const UserEventDetails = () => {
   return (
-    <div>UserEventDetails</div>
+    <div>
+      <div>
+        <h1 className='text-6xl font-sans font-semibold pad-1 text-amber-950'>Team Details</h1> <br /><hr /><br />
+        
+        <ul>
+
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Your Teams : </strong>{}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Team Code : </strong>{}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Other Members : </strong>{}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Participated in : </strong>{}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Restriction Status : </strong>{}</p></li>
+      
+        </ul>
+      </div>
+
+      <br /> <hr /> <br />
+
+      {/* <div>
+        <p className='text-4xl font-semibold'>Participations</p>
+        <p>Events you have participated in - </p>
+      </div> */}
+
+    </div>
+
   )
 }
 
--- src/Components/Dashboard/UserProfile.jsx
+++ src/Components/Dashboard/UserProfile.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-const UserProfile = (user) => {
+import './dashboard.css';
+const UserProfile = ({user}) => {
      console.log(user)
   return (
-    <div>UserProfile
-     <p className=' flex '> Email verification status : {user.email_verified_at == null ? <div>
-           <Link to="/verify" className=' pl-2 text-orange-600'>Verify Email</Link>
-     </div> : "Verified" 
-     } </p>
+    <div className=''>
+      
+      <div>
+        <p className='text-6xl font-sans font-semibold pad-1 text-amber-950'>User Profile</p> <br /> <hr /> <br />
+        
+        <ul>
+          <li><p className='flex p-pad font-sans text-2xl '><strong className="pad-2 profile-text"> Email Verification Status:</strong>{user.email_verified_at == null ?
+          <div>
+           <Link to="/verify" className='verify-btn'> Verify Email</Link>
+          </div> : "Verified"}
+        
+          </p></li>
+
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>ID : </strong>{user.id}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Name : </strong>{user.name}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Email : </strong>{user.email}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>Phone Number : </strong>{}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>College Name : </strong>{}</p></li>
+        <li><p className='p-pad font-sans text-2xl '><strong className='profile-text-2'>User QR : </strong>{}</p></li>
+        <li><p className='p-pad-2 font-sans text-2xl '><strong className='profile-text-2'>Account Created on : </strong>{}</p></li>
+      
+        </ul>
+      </div>
+      
+      <br /> <hr /> <br />
+
+      
+
     </div>
   )
 }
